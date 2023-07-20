9ec0df82a0a276b330a1bbfca856c73b22a5029b
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-20 23:33:29 +0530

    missingnumberproblem in java

---
+++ missingnumber.java
@@ -0,0 +1,26 @@
+public class missingnumber{
+    public static void main(String[] args) {
+        int[] arr1 = {1,3,4,5,6};   // sample input
+
+        int miss = 0;// this is for the size of the numbers that are missed in the arr1.initializing it with 0.
+
+        int arr2[] = new int[miss];
+    }
+}
+
+
+
+
+// Question 2: Find missing numbers in array -
+
+// Skill Mapping: Basic
+
+// Description: Consider an array with N number of consecutive integers, in which few numbers are missing. Find and list down these missing numbers using Java.
+
+// Sample input: arrNum = {1, 2, 4, 5, 6}
+
+// Output: 3
+
+// Sample input: arrNum = {1, 2, 3, 5, 6, 8, 10, 11, 14}
+
+// Output: 4, 7, 9, 12, 13
