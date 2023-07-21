2575840c68ab073f6c4edeb2b157f20b271e4ba0
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-21 08:58:16 +0530

    patch2

--- missingnumber.java
+++ missingnumber.java
@@ -61,20 +61,3 @@ public class missingnumber{
 
     }
 }
-
-
-
-
-// Question 2: Find missing numbers in array -
-
-// Skill Mapping: Basic
-
-// Description: Consider an array with N number of consecutive integers, in which few numbers are missing. Find and list down these missing numbers using Java.
-
-// Sample input: arrNum = {1, 2, 4, 5, 6}
-
-// Output: 3
-
-// Sample input: arrNum = {1, 2, 3, 5, 6, 8, 10, 11, 14}
-
-// Output: 4, 7, 9, 12, 13
