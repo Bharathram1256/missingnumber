c9d547ca1218ec70b3b3aebc4908f185f30a7529
Bharathram1256 <maruboyinibharathkumar@gmail.com>
2023-07-21 08:09:10 +0530

    for the consecutive numbers in the array

--- missingnumber.java
+++ missingnumber.java
@@ -1,10 +1,39 @@
 public class missingnumber{
     public static void main(String[] args) {
-        int[] arr1 = {1,3,4,5,6};   // sample input
+        int[] arr1 = {1,3,4,5,6,8};   // sample input
+        //code to display the arr1.
+        for(int k = 0;k<arr1.length;k++){
+            System.out.println(arr1[k]);
+        }
 
-        int miss = 0;// this is for the size of the numbers that are missed in the arr1.initializing it with 0.
+        int miss = arr1[arr1.length-1]-arr1.length;// this is for the size of the numbers that are missed in the arr1.initializing it with 0.
+        int arr2[] = new int[miss]; //this is initilizing the arr2.
 
-        int arr2[] = new int[miss];
+        //loop will continue until (last element of the arr1-1) times
+        try {    
+        for(int i = 0,j = 0; i<arr1.length;i++) {
+            if(arr1[i]==i+1){
+                System.out.println("this"+i+"is in order.");
+
+            }
+            else{
+
+                arr2[j]=i+1;
+                j+=1;
+            }
+        } 
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("writer here");
+        }
+        //code to display the arr2.
+        for(int k=0;k<miss;k++){
+            System.out.println(arr2[k]);
+        }
+        
+        
+
     }
 }
 
